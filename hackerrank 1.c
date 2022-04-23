7a29a0e5cd4f5026402c003029fe78bafa9db44d
ra7465 <103992155+ra7465@users.noreply.github.com>
2022-04-24 00:05:27 +0530

    Add files via upload

---
+++ hackerrank 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+float tractLand,tractLandAcred;
+scanf("%f",&tractLand);
+tractLandAcred=(float)tractLand/43560;
+printf("%.2f sq.ft is equal to %.2f acres",tractLand,tractLandAcred);
+return 0;
+}
